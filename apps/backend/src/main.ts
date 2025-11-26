5157abd3a9c8a69c80748de6cec8d7df21bf9312
ShackLin <g0972222165@gmail.com>
2025-11-26 14:43:38 +0800

    CorrectMainTs

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_ORIGIN?.split(',') ?? ['http://localhost:3000'],
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: false,
