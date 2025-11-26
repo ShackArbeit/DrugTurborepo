942192dcd8ff6cf02dcbd6c99281359210ee3031
ShackLin <g0972222165@gmail.com>
2025-11-26 09:17:20 +0800

    ChaneLink

--- apps/frontend/lib/apollo-client.tsx
+++ apps/frontend/lib/apollo-client.tsx
@@ -1,53 +1,3 @@
-// 'use client'; 
-// import { ApolloClient, InMemoryCache, HttpLink} from '@apollo/client'
-// import { ApolloProvider } from '@apollo/client';
-// import { setContext } from '@apollo/client/link/context'
-// import {onError} from '@apollo/client/link/error'
-// import React from 'react'
-
-// const httpLink = new HttpLink({
-//   uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || 'http://localhost:3001/graphql',
-//   fetch
-// })
-
-// const authLink = setContext((_, { headers }) => {
-//   const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null
-//   return {
-//     headers: {
-//       ...headers,
-//       Authorization: token ? `Bearer ${token}` : '', 
-//     },
-//   }
-// })
-
-// const errorLink = onError(({ graphQLErrors})=>{
-//      if(typeof window==='undefined') return 
-//      const pathname = window.location.pathname
-//      const isAuthPage = pathname.startsWith('/login') || pathname.startsWith('/register')
-//      if(graphQLErrors){
-//           for(const err of graphQLErrors){
-//               if(err.extensions?.code ==='UNAUTHENTICATED' && !isAuthPage){
-//                    localStorage.removeItem('token');
-//                    document.cookie = 'token=; Max-Age=0; path=/';
-//                    window.location.href = '/login';
-//                    break
-//               }
-//           }
-//      }
-// })
-
-// const client = new ApolloClient({
-//   link: errorLink.concat(authLink.concat(httpLink)),
-//   cache: new InMemoryCache(),
-// })
-
-// export default function ApolloClientProvider({ children }: { children: React.ReactNode }) {
-//   return (
-//     <ApolloProvider client={client}>
-//       {children}
-//     </ApolloProvider>
-//   )
-// }
 'use client';
 
 import React from 'react';
@@ -62,7 +12,7 @@ import { setContext } from '@apollo/client/link/context';
 import { onError } from '@apollo/client/link/error';
 
 const httpLink = new HttpLink({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || 'http://localhost:3001/graphql',
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || 'http://localhost:4000',
   fetch,
 });
 
